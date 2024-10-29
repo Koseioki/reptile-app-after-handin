360b69f55832f3a26d09d3963c5dbaf913530eb3
AntiKiljoy <hstconnor@gmail.com>
2024-10-29 13:31:12 +0100

    Navigation Update
    The Icon sits above the text on the nav :)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,43 @@
 import { NavLink } from "react-router-dom";
-import home from "../images/home.svg"
-import discover from "../images/discover.svg"
-import post from "../images/post.svg"
-import info from "../images/info.svg"
-import profile from "../images/profile.svg"
+import home from "../images/home.svg";
+import discover from "../images/discover.svg";
+import post from "../images/post.svg";
+import info from "../images/info.svg";
+import profile from "../images/profile.svg";
 
 export default function Nav() {
-    return (
-        <nav>
-                        
-
-            <NavLink to="/"><div className="nav-dual-coding"><img src={home} />Home</div></NavLink>
-            <NavLink to="/create"><img src={discover} /> Discover</NavLink>
-            <NavLink to="/profile"><img src={post} />Post</NavLink>
-            <NavLink to="/profile"><img src={info} />Info</NavLink>
-            <NavLink to="/profile"><img src={profile} />Profile</NavLink>
-        </nav>
-    );
+  return (
+    <nav>
+      <NavLink to="/">
+        <div className="nav-dual-coding">
+          <img src={home} alt="Home Icon" />
+          <span>Home</span>
+        </div>
+      </NavLink>
+      <NavLink to="/create">
+        <div className="nav-dual-coding">
+          <img src={discover} alt="Discover Icon" />
+          <span>Discover</span>
+        </div>
+      </NavLink>
+      <NavLink to="/profile">
+        <div className="nav-dual-coding">
+          <img src={post} alt="Post Icon" />
+          <span>Post</span>
+        </div>
+      </NavLink>
+      <NavLink to="/profile">
+        <div className="nav-dual-coding">
+          <img src={info} alt="Info Icon" />
+          <span>Info</span>
+        </div>
+      </NavLink>
+      <NavLink to="/profile">
+        <div className="nav-dual-coding">
+          <img src={profile} alt="Profile Icon" />
+          <span>Profile</span>
+        </div>
+      </NavLink>
+    </nav>
+  );
 }
