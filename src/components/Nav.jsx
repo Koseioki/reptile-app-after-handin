f1598c99a04af9250ff6f6bfd79db1c15c3ed474
Kosei Oki <koseioki1225@gmail.com>
2024-10-29 13:06:14 +0100

    I tried

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,20 @@
 import { NavLink } from "react-router-dom";
+import home from "../images/home.svg"
+import discover from "../images/discover.svg"
+import post from "../images/post.svg"
+import info from "../images/info.svg"
+import profile from "../images/profile.svg"
 
 export default function Nav() {
     return (
         <nav>
-            <NavLink to="/">Posts</NavLink>
-            <NavLink to="/create">Create</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
+                        
+
+            <NavLink to="/"><div className="nav-dual-coding"><img src={home} />Home</div></NavLink>
+            <NavLink to="/create"><img src={discover} /> Discover</NavLink>
+            <NavLink to="/profile"><img src={post} />Post</NavLink>
+            <NavLink to="/profile"><img src={info} />Info</NavLink>
+            <NavLink to="/profile"><img src={profile} />Profile</NavLink>
         </nav>
     );
 }
