dccfecc864ba2c09faf344dd3f2672f0faa00897
Kosei Oki <koseioki1225@gmail.com>
2024-11-19 18:48:27 +0100

    deployment thingy 2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/reptile-app/"}>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/reptile-app-after-handin/"}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
